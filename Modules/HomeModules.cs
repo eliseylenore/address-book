3dadf928e1e8bcf2a9d3f079198dadaf7b817678
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-10 08:48:15 -0800

    Add base code to HomeModules.cs

---
+++ Modules/HomeModules.cs
@@ -0,0 +1,18 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+using AddressBook.Objects;
+
+namespace AddressBook
+{
+  public class HomeModule: NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] =_=> {
+        return View["index.cshtml"];
+      };
+      
+    }
+  }
+}
