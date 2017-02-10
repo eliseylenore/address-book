4aea37d99e5ff5496bf7836a9698428640ee1ff5
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-10 09:08:12 -0800

    Create Number object

---
+++ Objects/Number.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic
+using System
+
+namespace AddressBook
+{
+  public class Number
+  {
+    private string _number;
+  }
+
+  public Number(string number)
+  {
+    _number = number;
+  }
+
+  public string GetNumber()
+  {
+    return _number;
+  }
+  public void SetName(string newNumber)
+  {
+    _number = newNumber;
+  }
+}
