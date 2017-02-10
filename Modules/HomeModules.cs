4e4670cbeb9af1717efa540677333541994f3a44
Elise St Hilaire <eliseylenore@gmail.com>
2017-02-10 11:00:31 -0800

    Add contact.cshtml and functionality for adding new contact

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -14,6 +14,20 @@ namespace AddressBook
         return View["index.cshtml", allContacts];
       };
       Get["/contact/form"] = _ => View["contact_form.cshtml"];
+      Post["/contact/new"] = _ => {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        var newContact = new Contact(Request.Form["name"]);
+        List<Number> contactNumbers = newContact.GetNumbers();
+        var newNumber = new Number(Request.Form["number"]);
+        contactNumbers.Add(newNumber);
+        List<Address> contactAddresses = newContact.GetAddresses;
+        var newAddress = new Address(Request.Form["first-line"], Request.Form["city"], Request.Form["state"], Request.Form["zip"]);
+        contactAddresses.Add(newAddress);
+        model.Add("numbers", contactNumbers);
+        model.Add("addresses", contactAddresses);
+        model.Add("contact", newContact);
+        return View["contact.cshtml", model];
+      }
     }
   }
 }
